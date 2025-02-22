b9c69a5a081e5f5162cb57ac5636c7cd09866d5e
rama-gorantla <104051556+rama-gorantla@users.noreply.github.com>
2025-02-22 11:33:27 +0530

    Update index.tsx

--- myApp/app/index.tsx
+++ myApp/app/index.tsx
@@ -13,7 +13,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#000',
+    backgroundColor: '#0A021F',
   },
   title: {
     fontSize: 24,
